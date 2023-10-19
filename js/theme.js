4f1579c4114ea67922a9d72636351176f248252b
GarryProhor <igorprohorchenko@gmail.com>
2023-10-18 20:51:16 -0800

    feat: Attack toggleTheme to theme button click event

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,17 @@
 'use strict';
 
+/**
+ * Toggles the theme between 'light' and 'dark'
+ * Manages the theme setting in the DOM and local storage
+ */
+const toggleTheme = function (){
+    const /** {string} */ currentTheme = document.documentElement.getAttribute('data-theme') || 'light';
+    const /** {string */ newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+}
+
 /*
 * Initialize the theme
  */
@@ -10,3 +22,10 @@ const /** {string} */ initialTheme = storedTheme ?? (systemThemeIsDark ? 'dark' 
 document.documentElement.setAttribute('data-theme', initialTheme);
 
 
+/*
+* Attack toggleTheme to theme button click event
+ */
+window.addEventListener("DOMContentLoaded", function (){
+    const /** {HTMLElement} */ $themeBtn = document.querySelector('[data-theme-btn]');
+    if ($themeBtn) $themeBtn.addEventListener('click', toggleTheme);
+})
