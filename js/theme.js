580846082d1f1eb2bada213b1b99f9fd8c941ac9
GarryProhor <igorprohorchenko@gmail.com>
2023-10-18 20:39:32 -0800

    feat: Initialize the theme

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/*
+* Initialize the theme
+ */
+
+const /** {string | null} */ storedTheme = localStorage.getItem('theme');
+const /** {Boolean} */ systemThemeIsDark = window.matchMedia('prefers-color-scheme: dark');
+const /** {string} */ initialTheme = storedTheme ?? (systemThemeIsDark ? 'dark' : 'light');
+document.documentElement.setAttribute('data-theme', initialTheme);
+
+
